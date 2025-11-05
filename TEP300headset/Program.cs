82d334fdb0f121a70bed1f7d42dbcd266f4ce9bd
nem <68953332+Dight67@users.noreply.github.com>
2025-11-05 21:19:27 +0100

    Logger added and guid changed

--- TEP300headset/Program.cs
+++ TEP300headset/Program.cs
@@ -2,6 +2,7 @@
 using SPTarkov.DI.Annotations;
 using SPTarkov.Server.Core.DI;
 using SPTarkov.Server.Core.Models.Spt.Mod;
+using SPTarkov.Server.Core.Models.Utils;
 using Range = SemanticVersioning.Range;
 
 namespace TEP300headset;
@@ -9,16 +10,16 @@ namespace TEP300headset;
 public record ModMetadata : AbstractModMetadata
 {
     public override List<string>? Incompatibilities { get; init; }
-    public override string ModGuid { get; init; } = "com.nekusi.tep300backport";
+    public override string ModGuid { get; init; } = "com.nem.tep300backport";
     public override string Name { get; init; } = "Peltor TEP-300 Backport + Fixes";
-    public override string Author { get; init; } = "Nekusi";
+    public override string Author { get; init; } = "nem";
     public override Dictionary<string, Range>? ModDependencies { get; init; } = new()
     {
-        { "com.wtt-commonlib", new Range("~2.0.0") }
+        { "com.wtt.commonlib", new Range("~2.0.0") }
     };
     public override string? Url { get; init; } = "https://github.com/Dight67/TEP-300-Backport-Fixes";
     public override List<string>? Contributors { get; init; }
-    public override SemanticVersioning.Version Version { get; init; } = new("1.0.0");
+    public override SemanticVersioning.Version Version { get; init; } = new("1.0.1");
     public override Range SptVersion { get; init; } = new("~4.0.3");
     public override string License { get; init; } = "MIT";
     public override bool? IsBundleMod { get; init; } = true;
@@ -26,7 +27,8 @@ public record ModMetadata : AbstractModMetadata
 
 [Injectable(TypePriority = OnLoadOrder.PostDBModLoader + 2)]
 public class TEP300HeadsetBackport(
-    WTTServerCommonLib.WTTServerCommonLib wttCommon
+    WTTServerCommonLib.WTTServerCommonLib wttCommon,
+    ISptLogger<TEP300HeadsetBackport> logger
 ) : IOnLoad
 {
     public async Task OnLoad()
@@ -36,6 +38,7 @@ public class TEP300HeadsetBackport(
 
         // Use WTT-CommonLib services
         await wttCommon.CustomItemServiceExtended.CreateCustomItems(assembly, Path.Join("db", "CustomItems"));
+        logger.Success("[TEP-300] TEP-300 backport and fixes loaded.");
         await Task.CompletedTask;
     }
 }
